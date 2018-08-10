0a6f1f1483a672696be3e55a6e1b9510cd76cfc1
Sergey Nagaitsev <nsv@roomberry.ru>
2018-08-10 15:25:21 +0300

    change server conf

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/ng-tech-ui-project'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('/dist/ng-tech-ui-project/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/ng-tech-ui-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
