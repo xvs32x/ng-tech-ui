2275e12b85083a2a51bca20c2d9ff712a4ffe24d
Sergey Nagaitsev <nsv@roomberry.ru>
2018-08-10 15:06:58 +0300

    Change deps for Heroku

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ng-tech-ui-project'));
+app.use(express.static(__dirname + '/dist/ng-tech-ui-project'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname, '/dist/ng-tech-ui-project/index.html'));
+
+  res.sendFile(path.join('/dist/ng-tech-ui-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
